df09df47e6fd04948e684128e0cf3f095aba4235
Ata <your-email@example.com>
2026-07-29 14:32:46 +0330

    feat: complete week 4 deliverables

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,13 +5,15 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
+    // Default ignores
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+
+    // Generated files
+    "coverage/**",
   ]),
 ]);
 
